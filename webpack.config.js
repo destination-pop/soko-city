6472416358966be1d0a3a592b176b18182b80bb1
Marie Luongo <marie.luongo@gmail.com>
2020-03-22 17:00:45 -0400

    resolving cherrypick

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,39 @@
 
 const webpack = require('webpack');
 const path = require('path');
+require('dotenv').config({path: __dirname + '/.env'})
+
+const {
+  NODE_ENV: mode
+} = process.env
+
+const devServer = {
+  contentBase: './public',
+  open: true
+}
 
 module.exports = {
+
+  mode,
+
+  devServer,
 
   entry: './src/index.js',
 
   output: {
-    path: path.resolve(__dirname),
-    filename: './public/bundle.js'
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/public/',
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  devtool: 'source-map',
+
+  watchOptions: {
+    ignored: /node_modules/
   },
 
   module: {
