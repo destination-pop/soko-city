2480659ca235bd1015dc042cefb8332597d1c88b
Renee <reneelberger@gmail.com>
2020-04-06 12:26:12 -0400

    updating instruction language

--- src/components/instructionOnSide.js
+++ src/components/instructionOnSide.js
@@ -13,12 +13,16 @@ const gameInstructions = () => {
         </li>
         <br />
         <li>
-          Use the player's motions to move the boxes in the sokoban puzzle
+        The player moves the boxes in the sokoban puzzle, you control the player's movements with the arrow keys
         </li>
         <br />
         <li>
           To complete the puzzle, move all the boxes in the puzzle to their goal
           points
+        </li>
+        <br />
+        <li>
+        If needed, you can use your mouse to click the reset button to start the level over again
         </li>
       </ul>
     </div>
