43e54cca1ba22a0518ae2df003fd520c43df6c41
Jasmin Soltani <jksoltani@gmail.com>
2020-04-07 15:02:23 -0400

    Bug Fix: End Scene & Last Text Box

--- src/scenes/EndScene.js
+++ src/scenes/EndScene.js
@@ -8,15 +8,46 @@ class EndScene extends Phaser.Scene {
     this.handleClick = this.handleClick.bind(this)
   }
 
-  create() {
-    this.add.text(110, 100, 'Success!', { fontSize: '50px' })
-    this.add.text(110, 175, 'You rescued Lola!', { fontSize: '20px' })
+  preload() {
+    this.load.image('lola', 'assets/sprites/chicken.png')
+    this.load.image('heart', 'assets/sprites/heart.png')
+    this.load.spritesheet('player', 'assets/sprites/playerSprites.png')
+  }
 
-    const reset = this.add.text(110, 300, 'Play again', {
-      fontSize: '30px'
-    })
-    reset.setInteractive({ useHandCursor: true })
-    reset.on('pointerdown', () => this.handleClick())
+  create() {
+    this.scene.bringToTop('EndScene')
+
+    //Display Images
+    this.player = this.add.sprite(235, 300, 'player',3).setScale(6)
+    this.lola = this.add.sprite(435, 320, 'lola').setScale(.1)
+    this.heart = this.add.sprite(335, 260, 'heart').setScale(0.05).setVisible(false)
+
+    //Add Text
+    this.add.text(220, 100, 'Success!', { fontSize: '50px' })
+    this.add.text(180, 175, 'You rescued Lola!', { fontSize: '30px' })
+
+    //Display Reset Button
+    this.time.delayedCall(1500,
+      () => {
+        this.heart.setVisible(true)
+        this.resetButton = this.add.text(240, 370, 'Play again', {
+          fontSize: '30px'
+        })
+        this.resetButton.setInteractive({ useHandCursor: true })
+        this.resetButton.on('pointerdown', () => this.handleClick())
+      }
+    )
+  }
+
+  update() {
+    this.moveImage(this.lola, -2)
+    this.moveImage(this.player, 2)
+  }
+
+  moveImage(image, speed) {
+    if (image.x < 325 || image.x > 345) {
+      image.x += speed
+    }
   }
 
   handleClick() {
